589e0893987132e3b46472cb7d842b1d97360efa
Jonathan Herrera <herrera.jonathan@uabc.edu.mx>
2022-03-09 12:47:57 -0800

    Clases de tipo de dato creadas

---
+++ src/main/kotlin/data/Dice.kt
@@ -0,0 +1,24 @@
+package data
+
+import kotlin.random.Random
+
+/**
+ * Clase que representa un dado de colores
+ */
+class Dice
+{
+    private val colors = DiceColor.values()
+    var topFace = roll()
+        private set
+    val FACES = 6
+
+    /**
+     * Tira el dado y regresa la cara que salió
+     */
+    fun roll() : String
+    {
+        val face = colors[Random.nextInt(0, (FACES - 1))]
+        topFace = face.color
+        return topFace
+    }
+}
---
+++ src/main/kotlin/data/DiceColor.kt
@@ -0,0 +1,15 @@
+package data
+
+/**
+ * Enumerador que contiene los colores
+ * permitidos dentro del juego
+ */
+enum class DiceColor(val color : String)
+{
+    RED("Red"),
+    GREEN("Green"),
+    YELLOW("Yellow"),
+    ORANGE("Orange"),
+    WHITE("White"),
+    BLUE("Blue");
+}
