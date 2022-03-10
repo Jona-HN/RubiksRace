7f853d73258590419d1ea1bacfeef4beff684570
Jonathan Herrera <herrera.jonathan@uabc.edu.mx>
2022-03-09 19:00:53 -0800

    Se cambió la estructura con la que se trabaja el grid del jugador (de arreglo a matriz). Además, se agregó un método que verifica si el usuario seleccionó una casilla adyacente a la casilla negra.

--- src/main/kotlin/model/PlayersGrid.kt
+++ src/main/kotlin/model/PlayersGrid.kt
@@ -1,6 +1,7 @@
 package model
 
 import data.DiceColor
+import java.awt.Point
 import kotlin.random.Random
 
 /**
@@ -8,10 +9,11 @@ import kotlin.random.Random
  */
 class PlayersGrid
 {
-    private val GRID_SIZE = 25;
-    var grid = arrayOfNulls<String>(GRID_SIZE)
+    // Número de filas y columnas
+    private val NO_OF_ROWS_N_COLS = 5;
+    var grid = Array (5) { arrayOfNulls<String>(NO_OF_ROWS_N_COLS) }
         private set
-    private var blackBoxindex = 0
+    private lateinit var blackBoxIndices : Point
 
     init
     {
@@ -29,28 +31,34 @@ class PlayersGrid
     {
         val colors = DiceColor.values()
         val repetitions = IntArray(colors.size)
+        var nextColor : String
         var nextColorIndex : Int
-        var colorReachedMaxRepetitions : Boolean
+        var nextColorReachedMaxRepetitions : Boolean
         var blackBoxAppeared = false
 
-        for (i in grid.indices)
+        for (row in 0 until NO_OF_ROWS_N_COLS)
         {
-            if (!blackBoxAppeared && Random.nextInt(1, 26) == 1)
+            for (col in 0 until NO_OF_ROWS_N_COLS)
             {
-                blackBoxindex = i
-                grid[blackBoxindex] = "Black"
-                blackBoxAppeared = true
-            }
-            else
-            {
-                do
+                if (!blackBoxAppeared && Random.nextInt(1, 26) == 1)
                 {
-                    nextColorIndex = Random.nextInt(colors.size)
-                    colorReachedMaxRepetitions = repetitions[nextColorIndex] > 3
-                } while (colorReachedMaxRepetitions)
+                    blackBoxIndices = Point(row, col)
+                    nextColor = "Black"
+                    blackBoxAppeared = true
+                }
+                else
+                {
+                    do
+                    {
+                        nextColorIndex = Random.nextInt(colors.size)
+                        nextColorReachedMaxRepetitions = repetitions[nextColorIndex] > 3
+                    } while (nextColorReachedMaxRepetitions)
 
-                repetitions[nextColorIndex]++
-                grid[i] = colors[nextColorIndex].color
+                    repetitions[nextColorIndex]++
+                    nextColor = colors[nextColorIndex].color
+                }
+
+                grid[row][col] = nextColor
             }
         }
     }
@@ -58,25 +66,67 @@ class PlayersGrid
     /**
      * Mueve la casilla indicada
      */
-    fun moveGridBox(firstBoxIndex : Int, secondBoxIndex : Int) : Boolean
+    fun moveGridBox(firstBoxIndices : Point, secondBoxIndices : Point) : Boolean
     {
         // Se comprueba que primero se haya seleccionado
         // la casilla que se quiere mover. Y que la segunda casilla
         // seleccionada sea la casilla negra
-        if (firstBoxIndex == blackBoxindex || secondBoxIndex != blackBoxindex)
+        if (firstBoxIndices == blackBoxIndices || secondBoxIndices != blackBoxIndices)
+        {
+            return false
+        }
+        // Se comprueba que la casilla seleccionada sea adyacente
+        // a la casilla negra
+        else if (!boxIsAdjacentToBlackBox(firstBoxIndices))
         {
             return false
         }
         // Una vez validado el movimiento, se realiza
         else
         {
-            val firstBoxIndexColor = grid[firstBoxIndex]
-            grid[secondBoxIndex] = firstBoxIndexColor
-            grid[firstBoxIndex] = "Black"
-            blackBoxindex = firstBoxIndex
+            val firstBoxIndexColor = grid[firstBoxIndices.x][firstBoxIndices.y]
+            grid[secondBoxIndices.x][secondBoxIndices.y] = firstBoxIndexColor
+            grid[firstBoxIndices.x][firstBoxIndices.y] = "Black"
+            blackBoxIndices = firstBoxIndices
 
             return true
         }
+    }
+
+    /**
+     * Verifica si la casilla que se quiere mover
+     * es adyacente a la casilla negra
+     */
+    private fun boxIsAdjacentToBlackBox(boxIndices : Point) : Boolean
+    {
+        val adjacentBoxesIndices = ArrayList<Point>(4);
+
+        // Casilla superior
+        val upperRow : Int = blackBoxIndices.x - 1
+        if (upperRow >= 0)
+        {
+            adjacentBoxesIndices.add(Point(upperRow, blackBoxIndices.y))
+        }
+        // Casilla inferior
+        val lowerRow = blackBoxIndices.x + 1
+        if (lowerRow <= NO_OF_ROWS_N_COLS - 1)
+        {
+            adjacentBoxesIndices.add(Point(lowerRow, blackBoxIndices.y))
+        }
+        // Casilla a la izquierda
+        val leftCol = blackBoxIndices.y - 1
+        if (leftCol >= 0)
+        {
+            adjacentBoxesIndices.add(Point(blackBoxIndices.x, leftCol))
+        }
+        // Casilla a la derecha
+        val rightCol = blackBoxIndices.y + 1
+        if (rightCol <= NO_OF_ROWS_N_COLS - 1)
+        {
+            adjacentBoxesIndices.add(Point(blackBoxIndices.x, rightCol))
+        }
+
+        return adjacentBoxesIndices.contains(boxIndices)
     }
 
     /**
@@ -87,16 +137,13 @@ class PlayersGrid
     {
         val result = StringBuilder()
 
-        for ((i, color) in grid.withIndex())
+        for (row in 0 until NO_OF_ROWS_N_COLS)
         {
-            if ((i + 1) % 5 == 0)
+            for (col in 0 until NO_OF_ROWS_N_COLS)
             {
-                result.append("$color \n")
+                result.append("${grid[row][col]} \t")
             }
-            else
-            {
-                result.append("$color \t")
-            }
+            result.append("\n")
         }
 
         return result.toString()
