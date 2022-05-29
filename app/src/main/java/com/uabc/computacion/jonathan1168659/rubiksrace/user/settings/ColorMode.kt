d05041bc1fed3e1ddf0f0c672c3a50fad954d39e
Jonathan Herrera <msjona28@outlook.com>
2022-05-28 20:47:31 -0700

    New: enum para el modo de color (brillante, pastel o daltónico).

---
+++ app/src/main/java/com/uabc/computacion/jonathan1168659/rubiksrace/user/settings/ColorMode.kt
@@ -0,0 +1,8 @@
+package com.uabc.computacion.jonathan1168659.rubiksrace.user.settings
+
+enum class ColorMode(val key: String)
+{
+    SHINY("shiny"),
+    PASTEL("pastel"),
+    COLORBLIND("colorblind")
+}
