5be29cf38bdb3441a5bcdbc0d00674b63a874ac9
Jonathan Herrera <msjona28@outlook.com>
2022-05-29 05:54:10 -0700

    Change: se cambió los colores de la clase Color por los declarados en colors.xml New: se agregó la variante pastel de los colores base. New: método que devuelve la variante pastel de un color brillante.

--- app/src/main/java/com/uabc/computacion/jonathan1168659/rubiksrace/data/DiceColor.kt
+++ app/src/main/java/com/uabc/computacion/jonathan1168659/rubiksrace/data/DiceColor.kt
@@ -1,19 +1,32 @@
 package com.uabc.computacion.jonathan1168659.rubiksrace.data
 
-import android.graphics.Color
+import com.uabc.computacion.jonathan1168659.rubiksrace.R.color.*
 
 /**
  * Enumerador que contiene los colores
  * permitidos dentro del juego
  */
-enum class DiceColor(val color : Int)
+enum class DiceColor(val shiny_color: Int, val pastel_color: Int)
 {
-    RED(Color.RED),
-    GREEN(Color.GREEN),
-    YELLOW(Color.YELLOW),
-    ORANGE(Color.rgb(240, 112, 14)),
-    WHITE(Color.WHITE),
-    // Color.BLUE no funciona en mi teléfono
-    // por alguna extraña razón
-    BLUE(Color.rgb(19, 115, 212))
+    RED(red, red_pastel),
+    GREEN(green, green_pastel),
+    YELLOW(yellow, yellow_pastel),
+    ORANGE(orange, orange_pastel),
+    WHITE(white, white),
+    BLUE(blue, blue_pastel);
+
+    companion object
+    {
+        fun equivalentColor(shinyColor: Int): Int
+        {
+            val color = values().find { it.shiny_color == shinyColor }
+
+            return when
+            {
+                color != null       -> color.pastel_color
+                shinyColor == black -> black_pastel
+                else                -> shinyColor
+            }
+        }
+    }
 }
