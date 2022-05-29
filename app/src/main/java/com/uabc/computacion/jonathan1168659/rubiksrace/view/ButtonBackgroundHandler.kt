2883971224e609978cb4c76720284ef20a36da67
Jonathan Herrera <msjona28@outlook.com>
2022-05-29 05:58:17 -0700

    Delete: eliminada la función changeImage. Ahora todo se hace con un sólo método. Change: ahora el fondo del botón (sea color o imagen) se cambia en base al parámetro colorMode. En caso de que sea color, este se resuelve desde este método con el parámetro resources.

--- app/src/main/java/com/uabc/computacion/jonathan1168659/rubiksrace/view/ButtonBackgroundHandler.kt
+++ app/src/main/java/com/uabc/computacion/jonathan1168659/rubiksrace/view/ButtonBackgroundHandler.kt
@@ -1,23 +1,41 @@
 package com.uabc.computacion.jonathan1168659.rubiksrace.view
 
+import android.content.res.Resources
 import android.widget.ImageButton
 import com.uabc.computacion.jonathan1168659.rubiksrace.R
-import com.uabc.computacion.jonathan1168659.rubiksrace.data.Figure
+import com.uabc.computacion.jonathan1168659.rubiksrace.data.*
+import com.uabc.computacion.jonathan1168659.rubiksrace.user.settings.ColorMode
 
 class ButtonBackgroundHandler
 {
     companion object
     {
-        fun changeBackgroundColor(button : ImageButton, color : Int)
+        fun changeBackgroundColor(resources: Resources, colorMode: String, button: ImageButton, color: Int)
         {
-            button.setBackgroundColor(color)
-            button.setImageResource(R.drawable.button_selector)
-        }
+            if (color == 0)
+            {
+                return
+            }
 
-        fun changeImage(button : ImageButton, color : Int)
-        {
-            val figure = Figure.getResourceFile(color)
-            button.setBackgroundResource(figure)
+            when (colorMode)
+            {
+                ColorMode.SHINY.key ->
+                {
+                    button.setBackgroundColor(resources.getColor(color, null))
+                }
+                ColorMode.PASTEL.key ->
+                {
+                    val equivalentColor = DiceColor.equivalentColor(color)
+                    button.setBackgroundColor(resources.getColor(equivalentColor, null))
+                }
+                ColorMode.COLORBLIND.key ->
+                {
+                    val figure = Figure.getResourceFile(color)
+                    button.setBackgroundResource(figure)
+                }
+            }
+
+            button.setImageResource(R.drawable.button_selector)
         }
     }
 }
